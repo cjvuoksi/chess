13db92200164bb895e7a04c36022a1322a28f6cd
cj <cjvuoksi@gmail.com>
2024-05-03 14:39:14 -0600

    Added Data records

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,7 +1,5 @@
 import chess.*;
 
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
