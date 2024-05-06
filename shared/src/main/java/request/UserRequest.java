28261178c03b4d0e7f58153655031be344477f3f
cj <cjvuoksi@gmail.com>
2024-05-06 15:47:03 -0600

    Working register

--- shared/src/main/java/request/UserRequest.java
+++ shared/src/main/java/request/UserRequest.java
@@ -8,4 +8,12 @@ public class UserRequest extends Request {
         this.username = username;
         this.password = password;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
