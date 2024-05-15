d29c1c1755cd6d18169a4f25f18a7bd7e08b1693
cj <cjvuoksi@gmail.com>
2024-05-15 13:22:41 -0600

    created socket sender

---
+++ server/src/main/java/socketservice/SocketSender.java
@@ -0,0 +1,32 @@
+package socketservice;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import webSocketMessages.serverMessages.ServerMessage;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class SocketSender {
+
+    private final Gson serializer = new Gson();
+
+    public void send(ServerMessage message, Session recipient) {
+        try {
+            if (recipient.isOpen()) {
+                recipient.getRemote().sendString(serializer.toJson(message));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void send(ServerMessage message, Collection<Session> recipients) {
+        recipients.forEach(session -> send(message, session));
+    }
+
+    public void send(ServerMessage message, Session exclude, Collection<Session> recipients) {
+        recipients.remove(exclude);
+        send(message, recipients);
+    }
+}
