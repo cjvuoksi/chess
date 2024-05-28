783c468fb074261708f787b79bb7cfccb4706b88
cj <cjvuoksi@gmail.com>
2024-05-28 13:43:22 -0500

    server facade add login and create

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,9 +3,11 @@ package client;
 import communication.HttpCommunicator;
 import model.UserData;
 import org.junit.jupiter.api.*;
+import request.CreateRequest;
 import request.RegisterRequest;
 import request.Request;
 import request.UserRequest;
+import response.CreateResponse;
 import response.LoginResponse;
 import response.Response;
 import server.Server;
@@ -21,7 +23,7 @@ public class ServerFacadeTests {
     private static UserData testUser = new UserData("test", "test", "test");
     private static String token;
     private static String token2;
-    private static int gameID;
+    private static int gameID = 0;
 
     @BeforeAll
     public static void init() {
@@ -31,6 +33,13 @@ public class ServerFacadeTests {
         serverFacade = new ServerFacadeExposer(port);
         http = serverFacade.getHTTPCommunicator();
         clear();
+    }
+
+    @BeforeEach
+    void setUpEach() {
+        if (token == null) {
+            register();
+        }
     }
 
     private static void clear() {
@@ -47,7 +56,12 @@ public class ServerFacadeTests {
     @Test
     @Order(1)
     @DisplayName("Register")
-    public void register() {
+    public void testRegister() {
+        assertNotNull(token);
+    }
+
+
+    private void register() {
         LoginResponse response = serverFacade.register(new RegisterRequest(testUser.username(), testUser.password(), testUser.email()));
         assertNull(response.getMessage());
         assertEquals(testUser.username(), response.getUsername());
@@ -55,13 +69,11 @@ public class ServerFacadeTests {
         token = response.getAuthToken();
     }
 
+
     @Test
     @Order(2)
     @DisplayName("Invalid Register")
     public void invalidRegister() {
-        if (token == null) {
-            register();
-        }
         LoginResponse response = serverFacade.register(new RegisterRequest(testUser.username(), testUser.password(), testUser.email()));
         assertNotNull(response.getMessage());
     }
@@ -70,9 +82,6 @@ public class ServerFacadeTests {
     @Order(3)
     @DisplayName("Login")
     public void login() {
-        if (token == null) {
-            register();
-        }
         LoginResponse response = serverFacade.login(new UserRequest(testUser.username(), testUser.password()));
         assertNull(response.getMessage());
         assertEquals(testUser.username(), response.getUsername());
@@ -92,14 +101,17 @@ public class ServerFacadeTests {
     @Order(5)
     @DisplayName("Create")
     public void create() {
-
+        CreateResponse response = serverFacade.createGame(new CreateRequest(token, "game"));
+        assertNull(response.getMessage());
+        assertNotEquals(0, response.getGameID());
+        gameID = response.getGameID();
     }
 
     @Test
     @Order(6)
     @DisplayName("Invalid Create")
     public void invalidCreate() {
-
+        CreateResponse response = serverFacade.createGame(new CreateRequest("bad", null));
     }
 
     @Test
