7e2334343f825f5ed3e196d0cb922dbfffbd3a5e
cj <cjvuoksi@gmail.com>
2024-06-06 15:21:22 -0600

    empty chess board display

--- server/src/main/resources/web/index.js
+++ server/src/main/resources/web/index.js
@@ -40,44 +40,7 @@ function send(path, params, method, authToken) {
         });
 }
 
-let ws;
 
-function wsStart() {
-    ws = new WebSocket('ws://localhost:8080/ws');
-
-    ws.onopen = () => {
-        console.log("New WS connection");
-        document.getElementById('ws').style.display = "block";
-    }
-    ws.onmessage = onmessage;
-    ws.onclose = () => {
-        console.log("WS closed");
-        document.getElementById('ws').style.display = "none";
-    }
-    ws.onerror = wsError;
-}
-
-function wsError(event) {
-    console.log(event);
-}
-
-function onmessage(event) {
-    const servermessage = event.data;
-    console.log(event.data);
-}
-
-function submitUserCommand() {
-    const type = document.getElementById('typeBox').value;
-    const command = document.getElementById('userCommandBox').value;
-
-    if (type && command) {
-        sendUserCommand(command)
-    }
-}
-
-function sendUserCommand(command) {
-    ws.send(command);
-}
 
 function displayRequest(method, endpoint, request) {
     document.getElementById('method').value = method;
@@ -118,6 +81,75 @@ function joinGame() {
     displayRequest('PUT', '/game', {playerColor: 'WHITE/BLACK/empty', gameID: 0});
 }
 
+let ws;
+
+function wsStart() {
+    ws = new WebSocket(`ws://localhost:${location.port}/ws`);
+
+    ws.onopen = () => {
+        console.log("New WS connection");
+        document.getElementById('ws').style.display = "block";
+    }
+    ws.onmessage = onmessage;
+    ws.onclose = () => {
+        console.log("WS closed");
+        document.getElementById('ws').style.display = "none";
+    }
+    ws.onerror = wsError;
+}
+
+function wsError(event) {
+    console.log(event);
+}
+
+let chess;
+
+function loadGame(game) {
+    const board = new Map()
+    game.game.board.chessBoard.map((item) => {
+        board.set(item[0], item[1]);
+    })
+    game.game.board.chessBoard = board;
+    chess = game.game;
+    console.log(chess);
+    displayBoard();
+}
+
+function onmessage(event) {
+    const servermessage = event.data;
+    var message = JSON.parse(servermessage);
+    console.log(message);
+
+    switch (message.serverMessageType) {
+        case 'LOAD_GAME':
+            loadGame(message.game);
+            break;
+        case 'ERROR':
+            displayServerMessage(message.errorMessage);
+            break;
+        default:
+            displayServerMessage(message.message)
+            break;
+    }
+}
+
+function displayServerMessage(message) {
+    document.getElementById("serverMessage").innerText = message;
+}
+
+function submitUserCommand() {
+    const type = document.getElementById('typeBox').value;
+    const command = document.getElementById('userCommandBox').value;
+
+    if (type && command) {
+        sendUserCommand(command)
+    }
+}
+
+function sendUserCommand(command) {
+    ws.send(command);
+}
+
 function displayUserCommand(type, request) {
     document.getElementById('typeBox').value = type;
     document.getElementById('endBox').value = "/ws";
@@ -131,13 +163,31 @@ function displayUserCommand(type, request) {
 
 function connect() {
     displayUserCommand("CONNECT", {
-        type: "CONNECT",
+        commandType: "CONNECT",
         teamColor: 'WHITE/BLACK',
         gameID: 0,
         authToken: document.getElementById('authToken').value
     })
 }
 
+const BOARD_SIZE = 8;
+
+function displayBoard() {
+    output = document.getElementById("board");
+    chess.board.chessBoard.forEach((value, key, map) => {
+
+    })
+    //TODO make interactive
+}
+
 function makeMove() {
-    displayUserCommand("MAKEMOVE",)
+    displayUserCommand("MAKEMOVE", {
+        commandType: "MAKEMOVE",
+    })
+}
+
+function leave() {
+    displayUserCommand("LEAVE", {
+        commandType: "LEAVE",
+    })
 }
